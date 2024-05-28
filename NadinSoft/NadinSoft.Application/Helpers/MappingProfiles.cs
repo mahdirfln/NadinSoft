656e099c2f9126b86976c5c171c0d2827eefb875
Mahdi Roshanfekr <mahdi.r.ln@gmail.com>
2024-05-28 15:48:46 +0330

    add automapper to application project

---
+++ NadinSoft/NadinSoft.Application/Helpers/MappingProfiles.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using NadinSoft.Application.CommandsQueries.Product.Commands.Create;
+using NadinSoft.Application.CommandsQueries.Product.Commands.Update;
+using NadinSoft.Domain.Entities;
+
+namespace NadinSoft.Application.Helpers
+{
+    public class MappingProfiles : Profile
+    {
+        public MappingProfiles()
+        {
+            CreateMap<CreateProduct, Product>();
+            CreateMap<UpdateProduct, Product>();
+        }
+    }
+}
