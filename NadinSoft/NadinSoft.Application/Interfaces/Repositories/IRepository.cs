e8193cd352fe037ff79875527d3aa3434d0e6163
Mahdi Roshanfekr <mahdi.r.ln@gmail.com>
2024-05-28 15:49:05 +0330

    add repository interfaces

---
+++ NadinSoft/NadinSoft.Application/Interfaces/Repositories/IRepository.cs
@@ -0,0 +1,23 @@
+﻿using NadinSoft.Domain.Entities;
+using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NadinSoft.Application.Interfaces.Repositories
+{
+    public interface IRepository<TEntity> where TEntity : BaseEntity
+    {
+        IQueryable<TEntity> Table();
+
+        Task<TEntity> GetById(int id);
+
+        Task<IList<TEntity>> GetAll();
+
+        Task<int> Add(TEntity entity);
+
+        Task<TEntity> Edit(TEntity entity);
+
+        Task Delete(TEntity entity);
+    }
+}
---
+++ NadinSoft/NadinSoft.Application/Interfaces/Repositories/IUserRepository.cs
@@ -0,0 +1,13 @@
+﻿using NadinSoft.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NadinSoft.Application.Interfaces.Repositories
+{
+    public interface IUserRepository : IRepository<User>
+    {
+    }
+}
