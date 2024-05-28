056decd10d5fceba9d38a1de86270892c3e8f48f
Mahdi Roshanfekr <mahdi.r.ln@gmail.com>
2024-05-28 15:34:09 +0330

    add entities

---
+++ NadinSoft/NadinSoft.Domain/Entities/Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NadinSoft.Domain.Entities
+{
+    public class Product : BaseEntity
+    {
+        public int UserId { get; set; }
+
+        public string Name { get; set; }
+
+        public DateTime ProduceDate { get; set; }
+
+        public string ManufacturePhone { get; set; }
+
+        public string ManufactureEmail { get; set; }
+
+        public bool IsAvailable { get; set; }
+
+    }
+}
